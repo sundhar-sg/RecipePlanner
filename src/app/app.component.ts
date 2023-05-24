abe68fa4abc2b958c5036b6358be484fa7443697
Sundhar S G <sundhargopal462@outlook.com>
2023-05-24 06:09:13 +0530

    Recipe Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RecipePlanner';
+  navbarCollapsed = true;
+
+  toggleNavbarCollapsing() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
 }
